eeb115c6132b6841aae4e23e155e1c8eaee2faaa
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-16 01:57:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,17 +34,19 @@ end
 
 def merge_data(array_of_name_hashes, array_of_full_hashes) #|[{first_name => "blake"}, {first_name => "ashley"}], [datas]|
   merged_array_of_hashes = []
-  array_of_name_hashes.each do |label, name| #first name, blake
-    array_of_full_hashes.each do |key, value|# blake, {height: 10,  weight: 10}
-      binding.pry
-        if key == name
-        indiv_new_hash[label] = name
-        value.each do |k, v|
-          indiv_new_hash[k] = v
+  array_of_name_hashes.each do |name_hash|
+    name_hash.each do |label, name_value| #first name,blake
+      array_of_full_hashes.each do |full_hash|# blake => {height: 10,  weight: 10}
+        full_hash.each do |name_key, stats| #blake, {height:10, weight:10}
+          if name_key == name_value
+            indiv_new_hash[label] = name_value
+            stats.each do |k, v|
+              indiv_new_hash[k] = v
+            end
+            merged_array_of_hashes<< indiv_new_hash
+          end
         end
-        merged_array_of_hashes<< indiv_new_hash
       end
-    end
+    merged_array_of_hashes
   end
-  merged_array_of_hashes
 end
